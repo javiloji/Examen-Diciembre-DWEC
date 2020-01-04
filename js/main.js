cb8004d3daeede0e82faa0ba260ec308a6c4fa6f
javiloji <javierlopera988@gmail.com>
2020-01-04 03:10:18 +0100

    Funcion nuevoEmpleado y evitar globalidad

--- js/main.js
+++ js/main.js
@@ -8,25 +8,25 @@
      * 
     */
 
-    let nuevaVentanaEnlace;
-    let enlace;
-
     /*
         Funcion deTodoUnPoco en la que se abre una nueva ventana al pulsar en el enlace.
     */
 
     let deTodoUnPoco = () => {
 
-        nuevaVentanaEnlace = window.open("", "", "width=500px, height= 400px,top=200px,left=500px");
+        let nuevaVentanaEnlace = window.open("", "", "width=500px, height= 400px,top=200px,left=500px");
         nuevaVentanaEnlace.document.write(`
         <html>
             <head>
                 <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
-                <title id="titulo">Javier Lopera Jimenez</title>
+                <title >De Todo Un Poco</title>
                 <script src="js/deTodoUnPoco.js"></script>
                 <link rel="stylesheet" href="css/style.css">
             </head>
             <body>
+                <noscript>
+                    <h1>Esta pagina necesita activar el contenido JavaScript</h1>
+                </noscript>
                 <h1>Javier Lopera Jiménez</h1>
                 <div id="container">
                     <div id = "div">
@@ -42,15 +42,57 @@
         );
         nuevaVentanaEnlace.document.close();
     }
+    /*
+        Funcion nuevoEmpleado en la que se abre una nueva ventana al pulsar en el botón con el formulario
+        para crear el empleado.
+    */
+    let nuevoEmpleado = () => {
 
-    let inicio = () => {
-
-        enlace = document.getElementById("detodounpoco");
-        enlace.addEventListener("click", deTodoUnPoco);
+        let nuevaVentanaEmpleado = window.open("", "", "");
+        nuevaVentanaEmpleado.document.write(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <script src="js/validar.js"></script>
+            <script src="js/Empleado.js"></script>
+            <link rel="stylesheet" href="css/style.css">
+            <title>Javier Lopera Jimenez</title>
+        </head>
+        <body>
+            <noscript>
+                <h1>Esta pagina necesita activar el contenido JavaScript</h1>
+            </noscript>
+            <h1>Javier Lopera Jiménez</h1>
+            <h2>Examen Diciembre 2019</h2>
+            <form id="formularioEmpleado">
+                <label for="nombre">Nombre del empleado: </label><input id="inputNombre" type="text"><span id="errorNombre" style="color:red;"></span>
+                <label for="fecha">Fecha de nacimiento: </label><input id="inputFecha" type="text"><span id="errorFecha" style="color:red;"></span>
+                <label for="dni">DNI:  </label><input  id="inputDni" type="text"><span id="errorDni" style="color:red;"></span>
+                
+            </form>
+            <button id="botonCrearEmpleado">Nuevo Empleado</button><br><br>
+            <button id="salir">Salir</button>
+            </body>
+        </html>
+        `);
+        nuevaVentanaEmpleado.document.close();
 
     }
 
-    
+    let inicio = () => {
+
+        let enlace = document.getElementById("detodounpoco");
+        enlace.addEventListener("click", deTodoUnPoco);
+
+        let empleado = document.getElementById("boton");
+        empleado.addEventListener("click", nuevoEmpleado);
+
+    }
+
+
 
     document.addEventListener("DOMContentLoaded", inicio);
 
